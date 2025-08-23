52b3f4cb27cdce04e3d5da3dd5ddcec2d76d8ac8
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-08-23 18:20:32 +0200

    chore: update formatting

--- tests/helios/util/util.guid.test.cpp
+++ tests/helios/util/util.guid.test.cpp
@@ -6,18 +6,18 @@ using Guid = helios::util::Guid;
 
 TEST(Guid, generateAndValue) {
 
-	for (int i = 0; i < 10; i++) {
-		Guid id = Guid::generate();
-		EXPECT_EQ(i+1, id.value());
-	}
+    for (int i = 0; i < 10; i++) {
+        Guid id = Guid::generate();
+        EXPECT_EQ(i + 1, id.value());
+    }
 }
 
 TEST(Guid, equals) {
 
-	Guid l = Guid::generate();
-	Guid r = Guid::generate();
+    Guid l = Guid::generate();
+    Guid r = Guid::generate();
 
-	EXPECT_TRUE(l == l);
-	EXPECT_TRUE(l != r);
-	EXPECT_FALSE(l == r);
+    EXPECT_TRUE(l == l);
+    EXPECT_TRUE(l != r);
+    EXPECT_FALSE(l == r);
 }

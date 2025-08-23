cfd596a59e33902483f05cd2b4d769ddae33425e
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-08-23 18:20:48 +0200

    feat: add pimpl implementation as reference

--- benchmarks/helios/util/Guid.benchmark.cpp
+++ benchmarks/helios/util/Guid.benchmark.cpp
@@ -1,20 +1,54 @@
 #include <benchmark/benchmark.h>
+#include <cstdint>
+#include <memory>
 
 import helios.util;
 
-static void BM_StringCreation(benchmark::State& state) {
-    for (auto _ : state)
-        helios::util::Guid::generate();
+struct GuidPimpl {
+    private:
+        struct GuidImpl {
+            uint64_t value;
+        };
+
+        explicit GuidPimpl(const uint64_t value)
+            : pImpl(std::make_unique<GuidImpl>()) {
+            this->pImpl->value = value;
+        }
+
+
+        std::unique_ptr<GuidImpl> pImpl;
+
+    public:
+        const uint64_t value() const {
+            return this->pImpl->value;
+        }
+
+        static GuidPimpl generate() {
+            static std::atomic<uint64_t> counter{ 1 };
+            return GuidPimpl(counter.fetch_add(1, std::memory_order_relaxed));
+        }
+
+        ~GuidPimpl() = default;
+};
+
+
+
+static void BM_GuidGenerate(benchmark::State& state) {
+
+    for (auto _ : state) {
+         benchmark::DoNotOptimize(helios::util::Guid::generate());
+    }
 }
-// Register the function as a benchmark
-BENCHMARK(BM_StringCreation);
+
+BENCHMARK(BM_GuidGenerate)->UseRealTime();
 
 // Define another benchmark
-static void BM_StringCopy(benchmark::State& state) {
-    std::string x = "hello";
-    for (auto _ : state)
-        std::string copy(x);
+static void BM_GuidPimplGenerate(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(GuidPimpl::generate());
+    }
+
 }
-BENCHMARK(BM_StringCopy);
+BENCHMARK(BM_GuidPimplGenerate)->UseRealTime();
 
 BENCHMARK_MAIN();

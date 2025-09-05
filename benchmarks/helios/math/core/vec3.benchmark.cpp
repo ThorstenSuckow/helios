5771686b8ba308904af5508b604a5293d00bc4e7
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-09-05 21:58:35 +0200

    feat: add cross() benchmark

--- benchmarks/helios/math/core/vec3.benchmark.cpp
+++ benchmarks/helios/math/core/vec3.benchmark.cpp
@@ -1,7 +1,14 @@
 #include <benchmark/benchmark.h>
 #include <glm/vec3.hpp>
+#include <glm/glm.hpp>
 
 import helios.math;
+
+glm::vec3 glm_vec_a{1, 2, 3};
+glm::vec3 glm_vec_b{4, 5, 6};
+
+helios::math::vec3 vec_a{1, 2, 3};
+helios::math::vec3 vec_b{4, 5, 6};
 
 
 static void BM_vec3Constructor(benchmark::State& state) {
@@ -17,6 +24,22 @@ static void BM_glm_vec3Constructor(benchmark::State& state) {
     }
 }
 BENCHMARK(BM_glm_vec3Constructor)->UseRealTime();
+
+
+static void BM_vec3Cross(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(helios::math::cross(vec_a, vec_b));
+    }
+}
+BENCHMARK(BM_vec3Cross)->UseRealTime();
+
+
+static void BM_glm_vec3Cross(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(glm::cross(glm_vec_a, glm_vec_b));
+    }
+}
+BENCHMARK(BM_glm_vec3Cross)->UseRealTime();
 
 
 BENCHMARK_MAIN();

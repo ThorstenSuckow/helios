eafa8ab725abccc91751bf682ed4e45bacf06d23
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-09-05 22:34:35 +0200

    feat: add vec3 dot-product

--- benchmarks/helios/math/core/vec3.benchmark.cpp
+++ benchmarks/helios/math/core/vec3.benchmark.cpp
@@ -11,6 +11,9 @@ helios::math::vec3 vec_a{1, 2, 3};
 helios::math::vec3 vec_b{4, 5, 6};
 
 
+/**
+ * Constructor
+ */
 static void BM_vec3Constructor(benchmark::State& state) {
     for (auto _ : state) {
         benchmark::DoNotOptimize(helios::math::vec3());
@@ -26,13 +29,15 @@ static void BM_glm_vec3Constructor(benchmark::State& state) {
 BENCHMARK(BM_glm_vec3Constructor)->UseRealTime();
 
 
+/**
+ * Cross
+ */
 static void BM_vec3Cross(benchmark::State& state) {
     for (auto _ : state) {
         benchmark::DoNotOptimize(helios::math::cross(vec_a, vec_b));
     }
 }
 BENCHMARK(BM_vec3Cross)->UseRealTime();
-
 
 static void BM_glm_vec3Cross(benchmark::State& state) {
     for (auto _ : state) {
@@ -41,5 +46,22 @@ static void BM_glm_vec3Cross(benchmark::State& state) {
 }
 BENCHMARK(BM_glm_vec3Cross)->UseRealTime();
 
+
+/**
+ * Dot
+ */
+static void BM_vec3Dot(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(helios::math::dot(vec_a, vec_b));
+    }
+}
+BENCHMARK(BM_vec3Dot)->UseRealTime();
+
+static void BM_glm_vec3Dot(benchmark::State& state) {
+    for (auto _ : state) {
+        benchmark::DoNotOptimize(glm::dot(glm_vec_a, glm_vec_b));
+    }
+}
+BENCHMARK(BM_glm_vec3Dot)->UseRealTime();
 
 BENCHMARK_MAIN();

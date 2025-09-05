2df588ab1c969ac4513de33e95a7f2236b8b17aa
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-09-05 23:56:15 +0200

    refactor: refactor camera as module interface

---
+++ src/helios/math/camera.cpp
@@ -0,0 +1,19 @@
+import helios.math;
+
+namespace helios::math {
+
+    mat4 lookAt(const vec3& eye,const vec3& center, const vec3& up) noexcept {
+        const auto z = (eye - center).normalize();
+        const auto x = cross(up, z).normalize();
+        const auto y = cross(z, x).normalize();
+
+        return mat4{
+            x[0], y[0], z[0], 0.0f,
+            x[1], y[1], z[1], 0.0f,
+            x[2], y[2], z[2], 0.0f,
+            -dot(x, eye), -dot(y, eye), -dot(z, eye), 1.0f,
+        };
+    }
+
+}
+

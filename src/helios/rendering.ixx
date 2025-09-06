6b32da99e46a000dfe223ea093ee9b01348fd4df
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-09-07 00:24:22 +0200

    feat: add helios.rendering.core:Mesh

---
+++ src/helios/rendering.ixx
@@ -0,0 +1,4 @@
+export module helios.rendering;
+
+
+export import helios.rendering.core;
---
+++ tests/helios/rendering/core/Mesh.test.cpp
@@ -0,0 +1,26 @@
+#include <gtest/gtest.h>
+
+
+import helios.rendering.core;
+import helios.math.core;
+
+namespace math = helios::math;
+namespace rendering = helios::rendering;
+
+
+TEST(MeshTest, data) {
+
+    auto mesh_data = rendering::MeshData();
+
+    mesh_data.vertices.push_back(
+        {{1.0f, 0.5f, 0.2f}}
+    );
+
+    auto mesh_data_ptr = std::make_shared<const rendering::MeshData>(mesh_data);
+
+    const auto mesh = rendering::Mesh{mesh_data_ptr};
+
+    EXPECT_EQ(mesh_data_ptr, mesh.mesh_data());
+}
+
+

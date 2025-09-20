82692aa8d3761ca3c0723cc4d92da4fc19b2bd81
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-09-20 21:14:14 +0200

    feat: update example code

--- examples/simple_cube_rendering/main.cpp
+++ examples/simple_cube_rendering/main.cpp
@@ -14,63 +14,29 @@ import helios.platform.input.types;
 import helios.platform.window.core;
 import helios.glfw.window;
 
-namespace heliosAppGlfw = helios::glfw::application;
-namespace heliosWinGlfw = helios::glfw::window;
-namespace heliosOpenGl = helios::rendering::opengl;
-namespace heliosInput = helios::platform::input;
-namespace heliosUtil = helios::util;
 
 int main() {
 
-    auto glfwGuard = heliosAppGlfw::GLFWRAIIGuard();
+    auto glfwGuard = helios::glfw::application::GLFWRAIIGuard();
 
-    // 1. create the rendering device
-    const auto opengl = std::make_unique<heliosOpenGl::OpenGLDevice>();
-
-    // 2. create the config for the main window
-    auto cfg = heliosAppGlfw::GLFWFactory::makeWindowCfg(
-        "helios - Simple Cube Renderer");
-
-    // 3. create the app.
-    const auto app = heliosAppGlfw::GLFWFactory::makeApplication(
-        opengl.get()
+    const auto app = helios::glfw::application::GLFWFactory::makeOpenGLApp(
+        "helios - Simple Cube Renderer"
     );
 
-    // 4. initialize the app
-    app->init();
-
-    // 5. create the main window and focus it
-    heliosWinGlfw::GLFWWindow& win = app->createWindow(cfg);
-
-    // [OPTIONAL] 5.1 set the window user pointer and register the framebuffersizecallback
-    win.setWindowUserPointer(std::make_unique<heliosWinGlfw::GLFWWindowUserPointer>(
-        opengl.get(), &win
-    ));
-    win.setFramebufferSizeCallback([] (GLFWwindow* nativeWin, const int width, const int height) {
-        if (const auto* ptr = static_cast<heliosWinGlfw::GLFWWindowUserPointer*>(glfwGetWindowUserPointer(nativeWin))) {
-            auto* win = dynamic_cast<heliosWinGlfw::GLFWWindow*>(ptr->window);
-            const auto& viewport = win->viewport();
-            std::cout << width << height;
-            ptr->renderingDevice->setViewport(viewport[0], viewport[1], width, height);
-        }
-    });
-
-
-    // 6. set focus to the window
-    app->focus(win);
+    auto win = dynamic_cast<helios::glfw::window::GLFWWindow*>(app->current());
 
 
     // get the InputManager
-    heliosInput::InputManager& inputManager = app->inputManager();
-    while (!win.shouldClose()) {
+    helios::platform::input::InputManager& inputManager = app->inputManager();
+    while (!win->shouldClose()) {
         inputManager.tick(0.0f);
 
-        if (inputManager.isKeyPressed(heliosInput::Key::ESC)) {
+        if (inputManager.isKeyPressed(helios::platform::input::Key::ESC)) {
             std::cout << "Key Pressed [ESC]" << std::endl;
-            std::ignore = win.setShouldClose(true);
+            std::ignore = win->setShouldClose(true);
         }
 
-        win.swapBuffers();
+        win->swapBuffers();
     }
 
     return EXIT_SUCCESS;

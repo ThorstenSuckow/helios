8ba7610e0cdf0d1ab3e0c647ec989fb79cc3a954
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-02 21:39:18 +0200

    feat: add MeshData module interface unit

---
+++ src/helios/rendering/model/MeshData.cpp
@@ -0,0 +1,56 @@
+module;
+
+#include <memory>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+module helios.rendering.model;
+
+import helios.util.log;
+import helios.rendering.core.Vertex;
+import helios.rendering.asset.shape.Shape;
+
+using namespace helios::rendering::asset::shape;
+using namespace helios::rendering::core;
+
+#define HELIOS_LOG_SCOPE "helios::rendering::model::MeshData"
+namespace helios::rendering::model {
+
+
+    MeshData::MeshData(
+        std::shared_ptr<const std::vector<Vertex>> vertices,
+        std::shared_ptr<const std::vector<unsigned int>> indices
+    ) : vertices_(std::move(vertices)),
+        indices_(std::move(indices)) {
+
+        if (!vertices_ || !indices_) {
+            const std::string msg = "MeshData constructor received a null shared pointer.";
+            logger_.error(msg);
+            throw std::invalid_argument(msg);
+        }
+    }
+
+
+    MeshData::MeshData(const Shape& shape) :
+        vertices_(shape.vertices),
+        indices_(shape.indices) {
+
+        if (!vertices_ || !indices_) {
+            const std::string msg = "MeshData constructor received a Shape with null shared pointer.";
+            logger_.error(msg);
+            throw std::invalid_argument(msg);
+        }
+    }
+
+
+    [[nodiscard]] const std::vector<Vertex>& MeshData::vertices() const noexcept {
+        return *vertices_;
+    }
+
+
+    [[nodiscard]] const std::vector<unsigned int>& MeshData::indices() const noexcept  {
+        return *indices_;
+    };
+
+}

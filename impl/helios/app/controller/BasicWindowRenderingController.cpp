4bb6be039c84b7171dc0a828a6635ce790bc912d
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-04 00:30:55 +0200

    feat: update module implementation unit

--- impl/helios/app/controller/BasicWindowRenderingController.cpp
+++ impl/helios/app/controller/BasicWindowRenderingController.cpp
@@ -10,20 +10,24 @@ import helios.window.event.FrameBufferResizeEvent;
 
 namespace helios::app::controller {
 
+
+    BasicWindowRenderingController::BasicWindowRenderingController(helios::rendering::RenderingDevice* renderingDevice):
+    renderingDevice_(renderingDevice)
+    {}
+
+
     void BasicWindowRenderingController::onFrameBufferResize(const helios::window::event::FrameBufferResizeEvent& e) {
         std::cout << e.width << " " << e.height << std::endl;
     }
 
-    Controller& BasicWindowRenderingController::subscribeTo(helios::event::Dispatcher& dispatcher) {
+
+    void BasicWindowRenderingController::subscribeTo(helios::event::Dispatcher& dispatcher) {
 
         dispatcher.subscribe<helios::window::event::FrameBufferResizeEvent>(
             [this](const helios::window::event::FrameBufferResizeEvent& e) {
                 onFrameBufferResize(e);
             }
         );
-
-        return *this;
-
     };
 
 };

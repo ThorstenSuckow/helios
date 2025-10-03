5162d9ce4aca17b11d431a51d3ab5dd701fe0b73
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-03 22:17:15 +0200

    refactor: move GLFWWindow test

---
+++ tests/ext/glfw/window/GLFWWindow.test.cpp
@@ -0,0 +1,25 @@
+#include <gtest/gtest.h>
+#include <type_traits>
+
+import helios.window;
+import helios.ext.glfw.window.GLFWWindow;
+import helios.ext.glfw.window.GLFWWindowConfig;
+
+using namespace helios::window;
+using namespace helios::ext::glfw::window;
+
+
+
+TEST(GLFWWindowConfigTest, data) {
+    EXPECT_TRUE((std::is_base_of_v<WindowConfig, GLFWWindowConfig>));
+    EXPECT_TRUE((std::is_convertible_v<GLFWWindowConfig*, WindowConfig*>));
+
+    const auto cfg = std::make_unique<GLFWWindowConfig>();
+
+    EXPECT_EQ(600, cfg->height);
+    EXPECT_EQ(800, cfg->width);
+    EXPECT_FALSE(cfg->frameBufferSizeCallback);
+    EXPECT_EQ(std::string{"helios - Window"}, cfg->title);
+}
+
+

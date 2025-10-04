d8f5e4261ca445bd7a755fb7a206134e09336d46
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-05 00:50:47 +0200

    feat: add implementation unit for Window

---
+++ impl/helios/window/Window.cpp
@@ -0,0 +1,43 @@
+module;
+
+#include <string>
+
+module helios.window.Window;
+
+import helios.util.Guid;
+import helios.window.WindowConfig;
+import helios.math.types;
+import helios.util.log.Logger;
+import helios.util.log.LogManager;
+
+
+namespace helios::window {
+
+
+    Window::Window(const WindowConfig& cfg) :
+        width_(cfg.width), height_(cfg.height), title_(cfg.title),
+        viewport_(cfg.viewport),
+        guid_(util::Guid::generate()){};
+
+
+    [[nodiscard]] const util::Guid& Window::guid() const noexcept {
+        return guid_;
+    }
+
+
+    [[nodiscard]] int Window::width() const noexcept {
+        return width_;
+    }
+
+
+    [[nodiscard]] int Window::height() const noexcept {
+        return width_;
+    }
+
+
+    [[nodiscard]] const math::vec4i& Window::viewport() const noexcept {
+        return viewport_;
+    }
+
+
+};

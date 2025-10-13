50652a7e3ccf6856e594dfe2a4b31110f7026940
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-13 20:37:42 +0200

    feat:rename Node to Renderable

--- src/helios/rendering/Renderable.cpp
+++ src/helios/rendering/Renderable.cpp
@@ -3,34 +3,36 @@ module;
 #include <memory>
 #include <stdexcept>
 
-module helios.rendering.model;
+module helios.rendering.Renderable;
 
 import helios.rendering.model.Mesh;
 import helios.rendering.model.Material;
 
 import helios.util.log;
 
-namespace helios::rendering::model {
+using namespace helios::rendering::model;
 
-    Node::Node(
+namespace helios::rendering {
+
+    Renderable::Renderable(
         std::unique_ptr<Mesh> mesh,
         std::unique_ptr<Material> material) :
         mesh_(std::move(mesh)),
         material_(std::move(material)) {
         if (!mesh_ || !material_) {
-            const std::string msg = "Node constructor received a null unique pointer.";
+            const std::string msg = "Renderable constructor received a null unique pointer.";
             logger_.error(msg);
             throw std::invalid_argument(msg);
         }
     }
 
 
-    [[nodiscard]] const Mesh& Node::mesh() const noexcept {
+    [[nodiscard]] const Mesh& Renderable::mesh() const noexcept {
         return *mesh_;
     };
 
 
-    [[nodiscard]] const Material& Node::material() const noexcept {
+    [[nodiscard]] const Material& Renderable::material() const noexcept {
         return *material_;
     };
 

318440667b4fe5c3e9534692ac8f8b60b3cee873
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-14 20:21:02 +0200

    feat: UniformValueMap uses array

--- src/helios/rendering/shader/UniformValueMap.cpp
+++ src/helios/rendering/shader/UniformValueMap.cpp
@@ -12,18 +12,23 @@ import helios.math.types;
 namespace helios::rendering::shader {
 
     void UniformValueMap::set(UniformSemantics uniformSemantics, const helios::math::mat4f& mat4f) {
-        map_.insert_or_assign(uniformSemantics, mat4f);
+        map_[std::to_underlying(uniformSemantics)] = mat4f;
     }
 
 
     const float* UniformValueMap::mat4f_ptr(UniformSemantics uniformSemantics) const noexcept {
 
-        if (auto it = map_.find(uniformSemantics); it != map_.end()) {
-            if (const helios::math::mat4f* matPtr = std::get_if<helios::math::mat4f>(&it->second)) {
-                return math::value_ptr(*matPtr);
-            }
+        const auto index = std::to_underlying(uniformSemantics);
+
+        if (index >= map_.size()) {
+            return nullptr;
         }
 
+        if (const auto el = map_[index]; el.has_value()) {
+            if (const auto* it = std::get_if<helios::math::mat4f>(&el.value())) {
+                return helios::math::value_ptr(*it);
+            }
+        }
 
         return nullptr;
     }

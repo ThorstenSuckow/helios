a70b03c3d35221f4119a3983fb097ea0d0b59fc1
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-14 09:28:18 +0200

    feat: make Material owner of Shader and MaterialData (see helios/#13)

--- src/helios/rendering/model/Material.cpp
+++ src/helios/rendering/model/Material.cpp
@@ -8,13 +8,37 @@ module helios.rendering.model.Material;
 
 namespace helios::rendering::model {
 
-    Material::Material(std::shared_ptr<const MaterialData> materialData) :
-    materialData_(std::move(materialData)) {
+
+    Material::Material(std::shared_ptr<const helios::rendering::shader::Shader> shader) :
+    shader_(std::move(shader)) {
+        if (!shader_) {
+            const std::string msg = "MaterialData constructor received a null Shader shared pointer.";
+            logger_.error(msg);
+            throw std::invalid_argument(msg);
+        }
+
+    }
+
+    Material::Material(
+        std::shared_ptr<const helios::rendering::shader::Shader> shader,
+        std::shared_ptr<const MaterialData> materialData) :
+        shader_(std::move(shader)),
+        materialData_(std::move(materialData)) {
+        if (!shader_) {
+            const std::string msg = "MaterialData constructor received a null Shader shared pointer.";
+            logger_.error(msg);
+            throw std::invalid_argument(msg);
+        }
         if (!materialData_) {
             const std::string msg = "Material constructor received a null MaterialData shared pointer.";
             logger_.error(msg);
             throw std::invalid_argument(msg);
         }
+    }
+
+
+    [[nodiscard]] const helios::rendering::shader::Shader& Material::shader() const noexcept {
+        return *shader_;
     }
 
 

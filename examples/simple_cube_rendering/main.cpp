34bc8b5847ea393b0b459bbc217b187d7540ac36
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-10-19 21:43:14 +0200

    feat: update cube example code

--- examples/simple_cube_rendering/main.cpp
+++ examples/simple_cube_rendering/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
+#include <numbers>
 
 
 import helios.math.types;
@@ -10,23 +11,44 @@ import helios.ext.glfw.window.GLFWWindow;
 
 import helios.input.InputManager;
 import helios.input.types.Key;
+
+import helios.rendering.RenderQueue;
+import helios.rendering.RenderPassFactory;
 import helios.rendering.model.MaterialData;
 import helios.rendering.model.MeshData;
 import helios.rendering.asset.shape.basic.Cube;
+
 import helios.util.io.BasicStringFileReader;
 
-import helios.ext.opengl.rendering.model.OpenGLShader;
-import helios.ext.opengl.rendering.model.OpenGLMaterial;
+import helios.ext.opengl.rendering.shader.OpenGLShader;
+import helios.rendering.model.Material;
 import helios.ext.opengl.rendering.model.OpenGLMesh;
-import helios.ext.opengl.rendering.model.OpenGLNode;
+import helios.ext.opengl.rendering.OpenGLRenderable;
+
+import helios.ext.opengl.rendering.shader.OpenGLUniformLocationMap;
+import helios.rendering.shader.UniformSemantics;
+
+import helios.scene.Scene;
+import helios.scene.SceneNode;
+import helios.scene.CullNoneStrategy;
+import helios.scene.Camera;
+
+import helios.math;
 
 using namespace helios::ext::glfw::app;
+using namespace helios::rendering;
 using namespace helios::rendering::model;
+using namespace helios::rendering::shader;
+using namespace helios::ext::opengl::rendering;
+using namespace helios::ext::opengl::rendering::shader;
 using namespace helios::ext::opengl::rendering::model;
 using namespace helios::rendering::asset::shape::basic;
 using namespace helios::input::types;
 using namespace helios::ext::glfw::window;
 using namespace helios::util::io;
+using namespace helios::scene;
+using namespace helios::math;
+
 
 int main() {
 
@@ -48,16 +70,41 @@ int main() {
         "./resources/cube.frag",
         basicStringFileReader
     );
-    auto materialData_ptr = std::make_shared<MaterialData>(shader_ptr);
-    auto material_ptr = std::make_unique<OpenGLMaterial>(materialData_ptr);
+
+    auto uniformLocationMap = std::make_unique<OpenGLUniformLocationMap>();
+    if (!uniformLocationMap->set(UniformSemantics::WorldMatrix, 1)) {
+        // intentionally left empty
+    }
+    shader_ptr->setUniformLocationMap(std::move(uniformLocationMap));
+
+    auto material_ptr = std::make_unique<Material>(shader_ptr);
 
     auto cube = Cube{};
+
+
     auto meshData = std::make_shared<const MeshData>(cube);
-    auto mesh_ptr = std::make_unique<OpenGLMesh>(meshData);
-    auto node = OpenGLNode(std::move(mesh_ptr), std::move(material_ptr));
+    auto mesh_ptr = std::make_shared<OpenGLMesh>(meshData);
+    auto cubeRenderable = std::make_shared<OpenGLRenderable>(
+        std::move(mesh_ptr), std::move(material_ptr)
+    );
+
+    auto frustumCullingStrategy = std::make_unique<CullNoneStrategy>();
+    auto rootSceneNode = std::make_unique<helios::scene::SceneNode>();
+    auto scene = std::make_unique<helios::scene::Scene>(std::move(frustumCullingStrategy));
+    if (!scene->addNode(std::move(rootSceneNode))) {/*intentionally left empty*/}
+    auto cubeSceneNode = std::make_unique<helios::scene::SceneNode>(std::move(cubeRenderable));
+    auto* cubeNode = scene->addNode(std::move(cubeSceneNode));
+    cubeNode->scale(vec3f(0.5f, 0.5f, 0.5f));
+    // camera
+    auto cam = std::make_unique<helios::scene::Camera>();
+    auto* cameraNode = scene->addNode(std::move(cam));
+    if (!cameraNode) {/*intentionally left empty*/}
+    const auto* camera = static_cast<const Camera*>(cameraNode);
 
 
 
+    float degrees = 0.0f;
+    float rad = 0.0f;
     while (!win->shouldClose()) {
         app->eventManager().dispatchAll();
 
@@ -67,11 +114,18 @@ int main() {
             std::cout << "Key Pressed [ESC]" << std::endl;
             win->setShouldClose(true);
         }
+        rad = helios::math::radians(degrees+=2.25);
+        cubeNode->rotate(helios::math::rotate(
+        helios::math::mat4f::identity(),
+            rad,
+        helios::math::vec3f(0.4f, 0.6, 0.2f)
+        ));
+        degrees = degrees > 360 ? 0 : degrees;
 
-        app->renderingDevice().beginRenderPass();
-
-        // model
-        node.draw();
+        // create the snapshot of the scene for the rendering pass
+        const auto& snapshot = scene->createSnapshot(*camera);
+        auto renderPass = RenderPassFactory::getInstance().buildRenderPass(snapshot);
+        app->renderingDevice().render(renderPass);
 
         win->swapBuffers();
     }
--- examples/simple_cube_rendering/resources/cube.vert
+++ examples/simple_cube_rendering/resources/cube.vert
@@ -2,7 +2,7 @@
 
 layout (location=0) in vec3 aPos;
 
-layout (location=76) uniform mat4 worldMatrix;
+layout (location=1) uniform mat4 worldMatrix;
 
 void main() {
     gl_Position = worldMatrix * vec4(aPos, 1.0f);

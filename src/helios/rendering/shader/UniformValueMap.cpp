e040ef85461009e978dd3d7c1619b591f4852046
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-05 23:08:36 +0100

    feat: add support for vec4f to UniformValueMap

--- src/helios/rendering/shader/UniformValueMap.cpp
+++ src/helios/rendering/shader/UniformValueMap.cpp
@@ -15,6 +15,9 @@ namespace helios::rendering::shader {
         map_[std::to_underlying(uniformSemantics)].emplace(mat4f);
     }
 
+    void UniformValueMap::set(const UniformSemantics uniformSemantics, const helios::math::vec4f& vec4f) noexcept {
+        map_[std::to_underlying(uniformSemantics)].emplace(vec4f);
+    }
 
     const float* UniformValueMap::mat4f_ptr(UniformSemantics uniformSemantics) const noexcept {
 
@@ -26,6 +29,23 @@ namespace helios::rendering::shader {
 
         if (const auto& el = map_[index]; el.has_value()) {
             if (const auto* it = std::get_if<helios::math::mat4f>(&el.value())) {
+                return helios::math::value_ptr(*it);
+            }
+        }
+
+        return nullptr;
+    }
+
+    const float* UniformValueMap::vec4f_ptr(UniformSemantics uniformSemantics) const noexcept {
+
+        const auto index = std::to_underlying(uniformSemantics);
+
+        if (index >= map_.size()) {
+            return nullptr;
+        }
+
+        if (const auto& el = map_[index]; el.has_value()) {
+            if (const auto* it = std::get_if<helios::math::vec4f>(&el.value())) {
                 return helios::math::value_ptr(*it);
             }
         }

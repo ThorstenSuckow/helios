fbf4a50732875c8ef5a8944f22b0004dcca5d5e1
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-08 20:46:42 +0100

    refactor: update example programs to work with updated API

--- examples/simple_cube_rendering/main.cpp
+++ examples/simple_cube_rendering/main.cpp
@@ -14,8 +14,10 @@ import helios.input.types.Key;
 
 import helios.rendering.RenderQueue;
 import helios.rendering.RenderPassFactory;
-import helios.rendering.model.MaterialData;
+import helios.rendering.model.Material;
+import helios.rendering.model.MaterialInstance;
 import helios.rendering.model.config.MeshConfig;
+import helios.rendering.model.config.MaterialProperties;
 import helios.rendering.model.config.PrimitiveType;
 import helios.rendering.asset.shape.basic.Cube;
 
@@ -79,7 +81,14 @@ int main() {
     }
     shader_ptr->setUniformLocationMap(std::move(uniformLocationMap));
 
-    auto material_ptr = std::make_unique<Material>(shader_ptr);
+    auto materialProperties = std::make_shared<MaterialProperties>(
+        helios::math::vec4f(1.0f, 0.0f, 1.0f, 1.0f)
+    );
+
+    auto material_ptr = std::make_shared<Material>(
+        shader_ptr, materialProperties
+    );
+    auto materialInstance = std::make_shared<MaterialInstance>(material_ptr);
 
     auto cube = Cube{};
 
@@ -88,7 +97,7 @@ int main() {
     );
     auto mesh_ptr = std::make_shared<OpenGLMesh>(cube, meshConfig);
     auto cubeRenderable = std::make_shared<OpenGLRenderable>(
-        std::move(mesh_ptr), std::move(material_ptr)
+        mesh_ptr, materialInstance
     );
 
     auto frustumCullingStrategy = std::make_unique<CullNoneStrategy>();

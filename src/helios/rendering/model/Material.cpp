d3412a2461267e303873f95748fc14d0d58d1bc1
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-08 20:45:05 +0100

    feat: refactor Material / MaterialData
    updated API allows for using Material as a base definition used by MaterialInstance

--- src/helios/rendering/model/Material.cpp
+++ src/helios/rendering/model/Material.cpp
@@ -6,52 +6,41 @@ module;
 module helios.rendering.model.Material;
 
 import helios.rendering.shader.Shader;
-import helios.rendering.model.MaterialData;
+import helios.rendering.model.config.MaterialProperties;
 import helios.rendering.shader.UniformValueMap;
 import helios.rendering.shader.UniformSemantics;
 
 using namespace helios::rendering::shader;
+using namespace helios::rendering::model::config;
 
 namespace helios::rendering::model {
 
 
     Material::Material(
         std::shared_ptr<const Shader> shader,
-        std::shared_ptr<const MaterialData> materialData) :
+        std::shared_ptr<const MaterialProperties> materialProperties) :
         shader_(std::move(shader)),
-        materialData_(std::move(materialData)) {
-        if (!shader_) {
-            const std::string msg = "MaterialData constructor received a null Shader shared pointer.";
+        materialProperties_(std::move(materialProperties)) {
+        if (!shader_ || !materialProperties_) {
+            const std::string msg = "Material constructor received a null shared pointer.";
             logger_.error(msg);
             throw std::invalid_argument(msg);
-        }
-        if (!materialData_) {
-            logger_.info("Material constructor received a null MaterialData shared pointer.");
         }
     }
 
 
-    [[nodiscard]] std::shared_ptr<const Shader> Material::shader() const noexcept {
+    std::shared_ptr<const Shader> Material::shader() const noexcept {
         return shader_;
     }
 
 
-    [[nodiscard]] std::shared_ptr<const MaterialData> Material::materialData() const noexcept {
-        return materialData_;
+    const MaterialProperties&  Material::materialProperties() const noexcept {
+        return *materialProperties_;
     }
 
-    /**
-     * @todo implement
-     */
-    void Material::writeUniformValues(UniformValueMap& uniformValueMap) const noexcept {
-
-        if (materialData_ != nullptr) {
-            uniformValueMap.set(UniformSemantics::MaterialColor, materialData_->color());
-        }
-
-
-    };
-
-
+    void Material::writeUniformValues(
+    helios::rendering::shader::UniformValueMap& uniformValueMap) const noexcept {
+        materialProperties_->writeUniformValues(uniformValueMap);
+    }
 
 }

b568f345a26bcfd7f46891fd0fa552b7123ab7b9
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-09 19:26:39 +0100

    feat: make MateriaProperties immutable

---
+++ src/helios/rendering/model/config/MaterialProperties.cpp
@@ -0,0 +1,39 @@
+
+module helios.rendering.model.config.MaterialProperties;
+
+
+import helios.math.types;
+import helios.rendering.shader.UniformValueMap;
+import helios.rendering.shader.UniformSemantics;
+
+
+namespace helios::rendering::model::config {
+
+
+    MaterialProperties::MaterialProperties(
+        const helios::math::vec4f baseColor,
+        const float roughness
+    ) noexcept : baseColor_(baseColor), roughness_(roughness){}
+
+
+    MaterialProperties MaterialProperties::withBaseColor(const helios::math::vec4f baseColor) const noexcept {
+        return MaterialProperties(
+            baseColor,
+            roughness_
+        );
+    }
+
+    MaterialProperties MaterialProperties::withRoughness(const float roughness) const noexcept {
+        return MaterialProperties(
+            baseColor_,
+            roughness
+        );
+    }
+
+
+    void MaterialProperties::writeUniformValues(shader::UniformValueMap& uniformValueMap) const noexcept {
+        uniformValueMap.set(helios::rendering::shader::UniformSemantics::MaterialBaseColor, baseColor_);
+        uniformValueMap.set(helios::rendering::shader::UniformSemantics::MaterialRoughness, roughness_);
+    }
+    ;
+}

f51ba302c71a89856ad148364727ef0ff042c8a9
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-10 23:15:34 +0100

    feat: enhance gamepad input handling in GLFWInputAdapter
    button states are now considered

--- src/ext/glfw/input/GLFWInputAdapter.cpp
+++ src/ext/glfw/input/GLFWInputAdapter.cpp
@@ -44,51 +44,70 @@ namespace helios::ext::glfw::input {
 
         int index = 0;
         while (gamepadMask != 0) {
+
             unsigned int id = gamepadMask & 1;
 
-            if (!id) {
-                // update iteration state
-                goto next_it;
-            }
-
-            {
+            if (id) {
                 // update gamepad
                 const auto gamepadId = static_cast<helios::input::types::Gamepad>(pow(2, index));
 
                 int glfwGamepadId = helios::ext::glfw::input::GLFWGamepadLookup::from(gamepadId);
-                int count;
-                const float* axes = glfwGetJoystickAxes(glfwGamepadId, &count);
 
-                if (count != 6) {
-                    logger_.warn(std::format(
-                        "Querying gamepad {0} failed, retrieved {1} input states",
-                        std::to_underlying(gamepadId) >> 1, count)
+                if (glfwJoystickIsGamepad(glfwGamepadId)) {
+                    GLFWgamepadstate state;
+                    glfwGetGamepadState(glfwGamepadId, &state);
+
+                    const float* axes = state.axes;
+                    const unsigned char* buttons = state.buttons;
+
+                    gamepadStates_[index].updateAxes(
+                        axes[GLFW_GAMEPAD_AXIS_LEFT_X],
+                        // invert left Y, glfw uses down:1, up:-1, we need donw:-1, up:1
+                        -axes[GLFW_GAMEPAD_AXIS_LEFT_Y],
+                        axes[GLFW_GAMEPAD_AXIS_RIGHT_X],
+                        // invert right Y, same logic as left Y
+                        -axes[GLFW_GAMEPAD_AXIS_RIGHT_Y],
+                        // normalize trigger values, since glfw returns them in the range
+                        // [-1, 1]
+                        axes[GLFW_GAMEPAD_AXIS_LEFT_TRIGGER]/2 + 0.5f ,
+                        axes[GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER]/2 + 0.5f,
+
+                        // button section
+                        !!buttons[GLFW_GAMEPAD_BUTTON_A],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_B],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_X],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_Y],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_START],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_BACK],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_GUIDE],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_LEFT_BUMPER],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_LEFT_THUMB],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_RIGHT_THUMB],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_DPAD_UP],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_DPAD_RIGHT],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_DPAD_DOWN],
+                        !!buttons[GLFW_GAMEPAD_BUTTON_DPAD_LEFT]
                     );
+
+                } else {
+                    logger_.warn("Querying gamepad {0} as gamepad failed");
                     // could also be a reset() method in GamepadState-object,
                     // but since we use it only once a manual assignment will do for now
-                    gamepadStates_[index].updateAxes(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+                    gamepadStates_[index].updateAxes(
+                        0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+                        false, false, false, false, false,
+                        false, false, false, false,
+                        false, false, false, false,
+                        false, false
 
-                    goto next_it;
+                    );
                 }
 
-                gamepadStates_[index].updateAxes(
-                    axes[0],
-                    // invert left Y, glfw uses down:1, up:-1, we need donw:-1, up:1
-                    -axes[1],
-                    axes[2],
-                    // invert right Y, same logic as left Y
-                    -axes[3],
-                    // normalize trigger values, since glfw returns them in the range
-                    // [-1, 1]
-                    axes[4]/2 + 0.5f ,
-                    axes[5]/2 + 0.5f
-                );
-            }
-            // end
+            } // if id
 
-            next_it:
-                index++;
-                gamepadMask >>= 1;
+            index++;
+            gamepadMask >>= 1;
         }
 
 

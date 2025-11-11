eef400ce6387f25e5ae430d4cdd15d7bc584387e
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-11 23:57:17 +0100

    test: add unit tests for vec2 constructors and operations

--- tests/helios/math/types/vec2.test.cpp
+++ tests/helios/math/types/vec2.test.cpp
@@ -3,6 +3,9 @@
 
 import helios.math;
 
+
+using namespace helios::math;
+
 namespace math = helios::math;
 
  struct test_data {
@@ -51,3 +54,46 @@ TEST(Vec2Tests, accessor) {
 
 }
 
+TEST(Vec2Tests, DefaultConstructorInitializesToZero) {
+    vec2f v;
+    EXPECT_EQ(v[0], 0.0f);
+    EXPECT_EQ(v[1], 0.0f);
+}
+
+TEST(Vec2Tests, ParameterizedConstructorInitializesCorrectly) {
+    vec2i v(3, 7);
+    EXPECT_EQ(v[0], 3);
+    EXPECT_EQ(v[1], 7);
+}
+
+TEST(Vec2Tests, ReadOnlyAccessWithinBounds) {
+    vec2d v(1.5, 2.5);
+    EXPECT_EQ(v[0], 1.5);
+    EXPECT_EQ(v[1], 2.5);
+}
+
+TEST(Vec2Tests, ReadWriteAccessWithinBounds) {
+    vec2i v(1, 2);
+    v[0] = 5;
+    v[1] = 10;
+    EXPECT_EQ(v[0], 5);
+    EXPECT_EQ(v[1], 10);
+}
+
+TEST(Vec2Tests, ScalarMultiplicationProducesCorrectResult) {
+    vec2f v(2.0f, 3.0f);
+    auto result = v * 2.0f;
+    EXPECT_EQ(result[0], 4.0f);
+    EXPECT_EQ(result[1], 6.0f);
+}
+
+TEST(Vec2Tests, DotProductComputesCorrectly) {
+    vec2i v1(1, 2);
+    vec2i v2(3, 4);
+    EXPECT_EQ(dot(v1, v2), 11);
+}
+
+TEST(Vec2Tests, AccessOutOfBoundsThrowsAssertion) {
+    vec2f v(1.0f, 2.0f);
+    EXPECT_DEATH(v[2], "vec2 - Index out of bounds.");
+}

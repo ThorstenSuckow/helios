e72605c8cff96812afe3c2d74c096a4b75ea9b8c
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-13 23:09:21 +0100

    feat: implement RenderPrototype class for shared renderable objects
    see helios/#46

---
+++ src/helios/rendering/RenderPrototype.cpp
@@ -0,0 +1,40 @@
+module;
+
+#include <stdexcept>
+#include <string>
+
+module helios.rendering.RenderPrototype;
+
+import helios.rendering.model.Mesh;
+import helios.rendering.model.Material;
+
+using namespace helios::rendering::model;
+
+namespace helios::rendering {
+
+    RenderPrototype::RenderPrototype(
+        std::shared_ptr<const Material> material,
+        std::shared_ptr<const Mesh> mesh
+    ) :
+    material_(std::move(material)),
+    mesh_(std::move(mesh)) {
+
+        if (!material_) {
+            throw std::invalid_argument("RenderPrototype received material nullptr");
+        }
+        if (!mesh_) {
+            throw std::invalid_argument("RenderPrototype received mesh nullptr");
+        }
+
+    }
+
+    const Mesh& RenderPrototype::mesh() const noexcept {
+        return *mesh_;
+    }
+
+
+    const Material& RenderPrototype::material() const noexcept {
+        return *material_;
+    }
+
+}

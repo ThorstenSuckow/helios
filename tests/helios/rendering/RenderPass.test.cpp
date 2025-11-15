ad66dc0128b430e29807944521fe47e53fdacddd
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-11-15 19:14:44 +0100

    refactor: RenderPass gracefully handles nullptr args
    add default fallbacks for nullptr, see helios/#74

---
+++ tests/helios/rendering/RenderPass.test.cpp
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include <memory>
+
+import helios.rendering.RenderPass;
+import helios.rendering.RenderQueue;
+import helios.rendering.RenderCommand;
+import helios.rendering.RenderPrototype;
+import helios.rendering.shader.UniformValueMap;
+import helios.rendering.shader.UniformSemantics;
+
+using namespace helios::rendering;
+using namespace helios::rendering::shader;
+
+TEST(RenderPassTest, HandlesArgsProperly) {
+
+    auto frameUniformValues = std::make_unique<UniformValueMap>();
+    frameUniformValues->set(UniformSemantics::MaterialRoughness, 1.0f);
+
+    auto objectUniformValues = std::make_unique<UniformValueMap>();
+    std::weak_ptr<RenderPrototype> weak;
+    auto renderCommand = std::make_unique<RenderCommand>(
+        weak,
+        std::move(objectUniformValues),
+        nullptr
+    );
+
+    auto queue = std::make_unique<RenderQueue>();
+    queue->add(std::move(renderCommand));
+
+    auto pass = RenderPass(std::move(queue), std::move(frameUniformValues));
+
+    EXPECT_EQ(pass.renderQueue().count(), 1);
+    const float* roughness = pass.frameUniformValues()
+                                 .float_ptr(UniformSemantics::MaterialRoughness);
+    ASSERT_NE(roughness, nullptr);
+    EXPECT_EQ(*roughness, 1.0f);
+
+}
+
+
+/**
+ * @see helios/#74
+ */
+TEST(RenderPassTest, HandlesNullRenderQueueGracefully) {
+    auto uniformValues = std::make_unique<UniformValueMap>();
+    auto pass = RenderPass(nullptr, std::move(uniformValues));
+    EXPECT_EQ(pass.renderQueue().count(), 0);
+}
+TEST(RenderPassTest, HandlesNullUniformValueMapGracefully) {
+    auto uniformValues = std::make_unique<RenderQueue>();
+    auto pass = RenderPass(std::move(uniformValues), nullptr);
+
+    EXPECT_EQ(pass.frameUniformValues().mat4f_ptr(UniformSemantics::WorldMatrix), nullptr);
+}
+TEST(RenderPassTest, HandlesBothNullArgsGracefully) {
+    auto uniformValues = std::make_unique<RenderQueue>();
+    auto pass = RenderPass(nullptr, nullptr);
+
+    EXPECT_EQ(pass.renderQueue().count(), 0);
+    EXPECT_EQ(pass.frameUniformValues().mat4f_ptr(UniformSemantics::WorldMatrix), nullptr);
+}
---
+++ tests/helios/rendering/shader/UniformValueMap.test.cpp
@@ -0,0 +1,19 @@
+#include <gtest/gtest.h>
+
+import helios.rendering.shader.UniformValueMap;
+import helios.rendering.shader.UniformSemantics;
+
+using namespace helios::rendering::shader;
+
+TEST(UniformValueMap, HandlesFloatValuesCorrect) {
+
+    auto map = UniformValueMap();
+
+    map.set(UniformSemantics::MaterialRoughness, 1.0f);
+
+    const float* roughness = map.float_ptr(UniformSemantics::MaterialRoughness);
+
+    ASSERT_NE(roughness, nullptr);
+
+    EXPECT_FLOAT_EQ(*roughness, 1.0f);
+}

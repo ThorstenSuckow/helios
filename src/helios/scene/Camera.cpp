9a9e9f74cb4321f340ed782efd5dca021d89b9af
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-12-08 22:39:39 +0100

    docs: update mat4 docs

--- src/helios/scene/Camera.cpp
+++ src/helios/scene/Camera.cpp
@@ -1,11 +1,14 @@
 module;
 
-#include <memory>
+#include <cassert>
 
 module helios.scene.Camera;
 
 import helios.math.types;
+import helios.math.transform;
 import helios.scene.SceneNode;
+
+
 
 namespace helios::scene {
 
@@ -15,6 +18,7 @@ namespace helios::scene {
     {}
 
     const helios::math::mat4f& Camera::projectionMatrix() const noexcept {
+        update();
         return projectionMatrix_;
     }
 
@@ -22,8 +26,22 @@ namespace helios::scene {
         return viewMatrix_;
     }
 
-    void Camera::setAspectRatio(const float aspectRatio) noexcept {
+    const Camera& Camera::setAspectRatio(const float aspectRatio) const noexcept {
+        needsUpdate_ = true;
         aspectRatio_ = aspectRatio;
+        return *this;
     }
 
+
+    const Camera& Camera::setPerspective(const float fovY, const float aspectRatio, const float zNear, const float zFar) const noexcept {
+
+        assert(zNear > 0 && "zNear must be positive");
+        assert(zFar > 0 && zFar > zNear && "zFar must be positive and greater than zNear");
+        fovY_ = fovY;
+        aspectRatio_ = aspectRatio;
+        zNear_ = zNear;
+        zFar_ = zFar;
+        needsUpdate_ = true;
+        return *this;
+    }
 } // namespace helios::scene

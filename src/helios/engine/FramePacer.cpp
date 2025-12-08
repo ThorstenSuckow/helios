cce2e0608ed37d9accd0d852f538d66105bf7089
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-12-08 23:25:26 +0100

    refactor: update module implementation units

---
+++ src/helios/engine/FramePacer.cpp
@@ -0,0 +1,53 @@
+module;
+
+#include <memory>
+#include <thread>
+#include <chrono>
+#include <cassert>
+
+module helios.engine.FramePacer;
+
+import helios.util.time.Stopwatch;
+import helios.engine.FrameStats;
+
+namespace helios::engine {
+
+    FramePacer::FramePacer(std::unique_ptr<helios::util::time::Stopwatch> stopwatch) :
+        stopwatch_(std::move(stopwatch)) {
+        assert(stopwatch_ && "FramePacer requires a valid Stopwatch (non-null)");
+    }
+
+    void FramePacer::setTargetFps(float fps) {
+        targetFps_ = fps;
+    }
+
+    float FramePacer::getTargetFps() const noexcept {
+        return targetFps_;
+    }
+
+    void FramePacer::beginFrame() {
+        stopwatch_->start();
+    }
+
+    FrameStats FramePacer::sync() {
+        float workTime = stopwatch_->elapsedSeconds();
+
+        float waitTime = 0.0f;
+        float totalTime = workTime;
+
+        if (targetFps_ > 0.0f) {
+            float targetTime = 1.0f / targetFps_;
+            if (targetTime > workTime) {
+                auto requestedWaitTime = targetTime - workTime;
+                auto sleepDuration = std::chrono::duration<float>(requestedWaitTime);
+                std::this_thread::sleep_for(sleepDuration);
+                totalTime = stopwatch_->elapsedSeconds();
+                waitTime = totalTime - workTime;
+            }
+        }
+
+        return FrameStats{ totalTime, workTime, waitTime };
+    }
+
+} // namespace helios::engine
+

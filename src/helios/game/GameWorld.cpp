92112eecdfcb7f826e9fa0f1a55c46bebb36f089
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-12-08 23:26:52 +0100

    refactor: update module implementation units

---
+++ src/helios/game/GameWorld.cpp
@@ -0,0 +1,66 @@
+module;
+
+#include <format>
+#include <memory>
+#include <unordered_map>
+
+module helios.game.GameWorld;
+
+import helios.game.GameObject;
+import helios.util.Guid;
+import helios.util.log.Logger;
+import helios.util.log.LogManager;
+
+namespace helios::game {
+
+    void GameWorld::update(float deltaTime) const noexcept {
+        for (auto& [guid, gameObject] : gameObjects_) {
+            gameObject->update(deltaTime);
+        }
+    }
+
+    helios::game::GameObject* GameWorld::addGameObject(std::unique_ptr<GameObject> gameObject) {
+        if (!gameObject) {
+            logger_.warn("Attempted to add null GameObject to GameWorld");
+            return nullptr;
+        }
+        if (gameObjects_.contains(gameObject->guid())) {
+            logger_.warn(std::format("GameObject with Guid {} already exists in GameWorld",
+                                     gameObject->guid().value()));
+            return nullptr;
+        }
+
+        auto* ptr = gameObject.get();
+        gameObjects_.emplace(gameObject->guid(), std::move(gameObject));
+
+        return ptr;
+    }
+
+    helios::game::GameObject* GameWorld::find(const helios::util::Guid& guid) {
+        if (auto it = gameObjects_.find(guid); it != gameObjects_.end()) {
+            return it->second.get();
+        }
+        return nullptr;
+    }
+
+    const helios::game::GameObject* GameWorld::find(const helios::util::Guid& guid) const {
+        if (auto it = gameObjects_.find(guid); it != gameObjects_.end()) {
+            return it->second.get();
+        }
+        return nullptr;
+    }
+
+    std::unique_ptr<helios::game::GameObject> GameWorld::removeGameObject(const GameObject& gameObject) {
+        auto node = gameObjects_.extract(gameObject.guid());
+
+        if (node.empty()) {
+            logger_.warn(std::format("Attempted to remove non-existent GameObject with Guid {} from GameWorld",
+                                     gameObject.guid().value()));
+            return nullptr;
+        }
+
+        return std::move(node.mapped());
+    }
+
+} // namespace helios::game
+

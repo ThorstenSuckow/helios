ae673e9c962d91e221055d6fe1275a15f508985a
ThorstenSuckow <thorsten@suckow-homberg.de>
2025-12-09 00:38:28 +0100

    fix: matrix-vector multiplication using wrong indices

--- tests/helios/math/types/mat4.test.cpp
+++ tests/helios/math/types/mat4.test.cpp
@@ -67,6 +67,28 @@ TEST(Mat4Tests, multiply) {
 }
 
 
+TEST(Mat4Tests, multiplyVector) {
+    math::mat4f M{
+        0, 1, 2, 3,
+        4, 5, 6, 7,
+        8, 9, 10, 11,
+        12, 13, 14, 15
+    };
+
+    math::vec4f v{1, 2, 3, 1};
+
+    math::vec4f B = M*v;
+
+    for (int row = 0; row < 4; row++) {
+        float sum = 0.0f;
+        for (int col = 0; col < 4; col++) {
+            sum += M(row, col) * v[col];
+        }
+        EXPECT_FLOAT_EQ(sum, B[row]);
+    }
+
+}
+
 TEST(Mat4Same, same) {
     math::mat4f M{
         0, 1, 2, 3,
